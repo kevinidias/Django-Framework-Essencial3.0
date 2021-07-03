8eaf6f369c10e488c87acab70a923520be28a4b0
kevinidias <kevin.dias.20160@gmail.com>
2021-07-03 19:36:03 -0300

    admin lte2

--- fusion/settings.py
+++ fusion/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'core',
+    'django_adminlte',
+    'django_adminlte_theme',
+    
 ]
 
 MIDDLEWARE = [
